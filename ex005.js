d2bcac415a27124f8ce89284042bc117d63badc6
Eric Uemura <96473905+uemura-eric@users.noreply.github.com>
2021-12-21 08:28:57 -0300

    Atualizado

---
+++ ex005.js
@@ -0,0 +1,13 @@
+var vel = 40
+if (vel > 50) {
+    console.log("Você foi multado!")
+} else {
+    console.log("Abaixo do limite")
+}
+
+var pais = "eua"
+if (pais == "brasil") {
+    console.log("Brasileiro")
+} else {
+    console.log("Estrangeiro")
+}
